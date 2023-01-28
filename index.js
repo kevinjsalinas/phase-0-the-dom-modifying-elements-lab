90c18d5e1b323b399862c8dd938a5b488652aede
Kevin Salinas <kevinsalinas155@gmail.com>
2023-01-28 12:58:05 -0500

    completed dom mod elements lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+const main = document.getElementById("main");
+
+main.remove();
+
+let newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id","victory");  
+
+newHeader.textContent = "KEVIN is the champion"; 
